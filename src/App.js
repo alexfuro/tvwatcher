0889a8294beca17546378490b150e99893eddb4c
AlexFuro <afurones43@gmail.com>
2018-07-22 18:43:23 -0400

    Add new show functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ShowList from './showlist';
+import ShowForm from './showform';
 
 const sample = [
   {
@@ -19,11 +20,27 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {shows: sample};
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
     this.toggleStatus = this.toggleStatus.bind(this);
   }
   save(shows) {
     this.setState({shows});
+  }
+  newShow(event){
+    const title = event.target.children[2].value;
+    const status = event.target.children[4].checked;
+
+    return { title, status };
+  }
+  clearForm(event){
+    event.target.children[2].value    = "";
+    event.target.children[4].checked = false;
+  }
+  addToList(show){
+    const shows = this.state.shows;
+    shows.push(show);
+    this.save(shows);
   }
   removeFromList(showId){
     const shows = this.state.shows;
@@ -40,6 +57,16 @@ class App extends Component {
   handleDelete(showId){
    this.removeFromList(showId);
   }
+  handleSubmit(event){
+    event.preventDefault();
+
+    const show = this.newShow(event);
+
+    if(show) {
+      this.addToList(show);
+      this.clearForm(event);
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -47,6 +74,7 @@ class App extends Component {
           <h1 className="App-title">The Watchlist!</h1>
         </header>
         <ShowList shows={this.state.shows} onDelete={this.handleDelete} toggleStatus={this.toggleStatus} />
+        <ShowForm onSubmit={this.handleSubmit}/>
       </div>
     );
   }
---
+++ src/showform.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './showform.css';
+
+class ShowForm extends Component {
+  render() {
+    return (
+      <form className="ShowForm" onSubmit={this.props.onSubmit}>
+         <legend>Add to Watchlist</legend>
+         <label>Title:</label>
+         <input type="text" name="title" placeholder="title" />
+
+         <label>Watched?</label>
+         <input type="checkbox" name="status" />
+
+         <input type="submit" id="submitShowForm" name="submitShow" value="Save" />
+       </form>
+    );
+  }
+}
+
+export default ShowForm;
