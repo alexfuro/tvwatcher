ac0cfd886e9747c4cf5136016c4af2bc014a4817
AlexFuro <afurones43@gmail.com>
2018-07-22 17:46:08 -0400

    Add basic showlist component

---
+++ src/showlist.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './showlist.css';
+import Show from './show';
+
+class WatchList extends Component {
+  render() {
+    const shows = this.props.shows;
+    return (
+      <div className="watchlist">
+      <table>
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Watch Status</th>
+                <th>Watch/Unwatched</th>
+                <th>Remove</th>
+            </tr>
+            </thead>
+            <tbody>
+            {shows.map((show,index)=>(
+                <Show show={show} key={index} id={index}}/>
+                ))}
+            </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default WatchList;
