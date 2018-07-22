f522e82ea6f54493d240a6a975fd10c6c0d095a4
AlexFuro <afurones43@gmail.com>
2018-07-22 17:45:41 -0400

    Add basic show component

---
+++ src/show.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './show.css';
+
+class Show extends Component {
+  render() {
+    const {show} = this.props;
+    return (
+      <tr>
+        <td>{show.title}</td>
+        <td>{show.status? "watched" : "unwatched"}</td>
+        <td><button>Change Status</button></td>
+        <td><button>Delete Book</button></td>
+      </tr>
+    );
+  }
+}
+
+export default Show;
