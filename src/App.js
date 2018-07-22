d311f5da7677dbbe9ea2bbb187a04bca129bf6c0
AlexFuro <afurones43@gmail.com>
2018-07-22 17:46:28 -0400

    Add showlist to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ShowList from './showlist';
+
+const sample = [
+  {
+    title: "Juni Taisen",
+    status: true
+  },{
+    title: "Mirai Nikki",
+    status: true
+  },{
+    title: "Boku no Hero",
+    status: false
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">The Watchlist!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ShowList shows={sample} />
       </div>
     );
   }
