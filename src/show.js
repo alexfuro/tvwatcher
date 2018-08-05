4cbae26622efe4ae6602815ed1c94198ef4a9355
AlexFuro <afurones43@gmail.com>
2018-08-05 08:15:16 -0400

    Change from table base presentation to div based

--- src/show.js
+++ src/show.js
@@ -5,12 +5,20 @@ class Show extends Component {
   render() {
     const {show} = this.props;
     return (
-      <tr>
-        <td>{show.title}</td>
-        <td>{show.status? "watched" : "unwatched"}</td>
-        <td><button onClick={() => this.props.onToggleStatus(this.props.id)}>Change Status</button></td>
-        <td><button onClick={() => this.props.onDelete(this.props.id)}className="deleteAction">Delete Show</button></td>
-      </tr>
+      <div className="show">
+        <span>{show.title}</span>
+        <ul class="show-edits">
+          <li>
+            {show.status? "watched" : "unwatched"}
+          </li>
+          <li>
+            <button onClick={() => this.props.onToggleStatus(this.props.id)}>Change Status</button>
+          </li>
+          <li>
+            <button onClick={() => this.props.onDelete(this.props.id)}className="deleteAction">Delete Show</button>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
--- src/showlist.js
+++ src/showlist.js
@@ -6,23 +6,12 @@ class WatchList extends Component {
   render() {
     const shows = this.props.shows;
     return (
-      <div className="watchlist">
-      <table>
-            <thead>
-            <tr>
-                <th>Title</th>
-                <th>Watch Status</th>
-                <th>Watch/Unwatched</th>
-                <th>Remove</th>
-            </tr>
-            </thead>
-            <tbody>
-            {shows.map((show,index)=>(
-                <Show show={show} key={index} id={index}
-                onDelete={this.props.onDelete} onToggleStatus={this.props.toggleStatus}/>
-                ))}
-            </tbody>
-        </table>
+      <div className="watch-list">
+        <h2>My List</h2>
+          {shows.map((show,index)=>(
+              <Show show={show} key={index} id={index}
+              onDelete={this.props.onDelete} onToggleStatus={this.props.toggleStatus}/>
+              ))}
       </div>
     );
   }
